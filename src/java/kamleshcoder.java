59eba7280aeca74995016f7ecf8e6dd6f390e998
Pravingautam <pravinjgautam123@gmail.com>
2023-08-17 15:48:46 +0530

    java program

---
+++ src/java/kamleshcoder.java
@@ -0,0 +1,11 @@
+public class kamleshcoder {
+    public static void main(String[] args) {
+        int hours=10;
+        int minutes=30;
+        int seconds=minutes*60;
+        int hourSeconds= hours *60 *60;
+         System.out. println("hour to seconds:"+hourSeconds);
+         System.out.println("minutes to seconds:"+ seconds);
+
+    }
+}
